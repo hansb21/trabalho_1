d898c0559611931f13b0d4dfc5dd5b4213e32f51
Hans Buss <hans.buss@mailfence.com>
2020-09-22 19:20:19 -0300

    Added main.cpp sample

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() 
+{
+    char xmlfilename[100];
+
+    std::cin >> xmlfilename;
+
+    /*
+    * Codigo =/
+    */
+
+    std::cout << xmlfilename << std::endl;
+
+    return (0);
+}
